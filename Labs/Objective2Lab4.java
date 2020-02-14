09aa4710dd29ec0c74bf0429e640f1afa622239d
Rich Wasek <rideburtonrw@gmail.com>
2020-02-14 15:30:07 -0700

    Completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main( String[] args ){
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("This output would result from numbers 15,6,2\n");
+    System.out.println(" " + average );
+  }
+}
