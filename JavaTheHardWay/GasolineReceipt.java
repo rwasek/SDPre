3f9390098db5bb74e854e1916c75946ec5c8a6bf
Rich Wasek <rideburtonrw@gmail.com>
2020-02-13 17:29:29 -0700

    Completed jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+    public static void main( String[] args ) {
+        System.out.println( "+------------------------+" );
+        System.out.println( "|                        |" );
+        System.out.println( "|      CORNER STORE      |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| 2020-02-13  05:25PM    |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Gallons:       10.870  |" );
+        System.out.println( "| Price/gallon: $ 2.42   |" );
+        System.out.println( "|                        |" );
+        System.out.println( "| Fuel total:  $ 26.31   |" );
+        System.out.println( "|                        |" );
+        System.out.println( "+------------------------+" );
+    }
+}
