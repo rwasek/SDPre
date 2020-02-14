1cf61af96148ea43b782ca4ac1d44bf110709218
Rich Wasek <rideburtonrw@gmail.com>
2020-02-14 10:30:59 -0700

    Completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.println("Name : Rich Wasek");
+    System.out.print("Age : 30\n"); // Just playing around with println vs \n
+    System.out.println("State"+" "+":"+" "+"New Jersey"); // Adding spaces the hard way :)
+  }
+}
