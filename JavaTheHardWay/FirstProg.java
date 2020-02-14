ab92dcb5e0069edd8370cfdc7170892c2d83550c
Rich Wasek <rideburtonrw@gmail.com>
2020-02-13 17:24:43 -0700

    Updated jthw ex 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,8 +1,8 @@
 public class FirstProg {
 
   public static void main(String[] args) {
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+    System.out.println("I, Rich Wasek, am determined to learn how to code");
+    System.out.println("Today's date is Thursday, February 13, 2020.");
   }
-  
+
 }
