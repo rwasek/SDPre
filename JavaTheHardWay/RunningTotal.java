5b3df58d48af61d2ae34fe1858e28d94551ced64
Rich Wasek <rideburtonrw@gmail.com>
2020-02-22 14:03:07 -0700

    Completed jthw ex 30

---
+++ JavaTheHardWay/RunningTotal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RunningTotal {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    int current = 11, total = 0; // leaving in current = 11 from the study drill
+
+    System.out.print("Type in a bunch of values and I'll add them up. ");
+    System.out.println("I'll stop when you type a zero.");
+
+    do {
+        System.out.print("Value: ");
+        current = keyboard.nextInt();
+        total += current;
+
+        if (current !=0 )
+           System.out.println("The total so far is: " + total);
+
+    } while (current != 0);
+
+    System.out.println("The final total is: " + total);
+  }
+}
