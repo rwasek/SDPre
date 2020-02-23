fa53210f6942fb3c54feeb62c729a56313468bca
Rich Wasek <rideburtonrw@gmail.com>
2020-02-22 22:07:41 -0700

    Completed Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count=0, sum=0;
+
+    while (count < 20) {
+      count = count + 1;
+      sum = count + sum;
+    } System.out.println(sum);
+
+  }
+}
